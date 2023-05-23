a80b4fea9d5c0b314bbb3b0b7b767d195abe87e0
grace-moon <grace.08de98@gmail.com>
2023-05-24 02:01:15 +0900

    add all

--- Unimaze/settings.py
+++ Unimaze/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-vy&^*iw24n_511etd-5jjm%%d*$!j@tym$w)7en^#a9uwd-(yq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
